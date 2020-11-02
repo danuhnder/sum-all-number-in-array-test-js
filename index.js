d8ada64fffd65140c94b08c9da51f8cc19d6d724
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-02 02:14:02 +0000

    functionality implemented

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let sum = 0;
+  array.forEach(x => {
+    if (Array.isArray(x)) {
+      sum += sumItems(x);
+    } else sum += x;
+  });
+  return sum;
+};
 
 module.exports = sumItems;
